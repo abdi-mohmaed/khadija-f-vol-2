f9790bf29011e318904679ec47762cfea83ed4f4
abdi-mohmaed <2115abdimoe@gmail.com>
2026-09-19 21:50:03 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/khadija-f-vol-2/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
